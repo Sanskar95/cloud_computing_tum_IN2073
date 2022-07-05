94d195beb04815c24b43c7b2152a9631f0965e9f
Sanskar Gupta <snskr95@gmail.com>
2022-07-05 19:26:40 +0200

    added ex3 folder

---
+++ EX3/product-descp-service/product_descp.js
@@ -0,0 +1,53 @@
+module.exports = function (options) {
+    //Import the mock data json file
+    const mockData = require('./MOCK_DATA.json');
+
+    //Add the patterns and their corresponding functions
+    this.add('role:product,cmd:getProductURL', productURL);
+    this.add('role:product,cmd:getProductName', productName);
+
+
+    //To DO: add the pattern functions and describe the logic inside the function
+
+}
+
+
+
+module.exports = function (options) {
+    //Import the mock data json file
+    const mockData = require('./MOCK_DATA.json');
+
+    //Add the patterns and their corresponding functions
+    this.add('role:product,cmd:getProductURL', productURL);
+    this.add('role:product,cmd:getProductName', productName);
+
+
+    //To DO: add the pattern functions and describe the logic inside the function
+    function productURL(args, done)
+    {
+        let productId = args.productId;
+        let bookURL = null; 
+        for(let i=0; i < mockData.length; i++)
+        {
+            if (mockData[i].product_id === productId)
+            {
+                bookURL = mockData[i].product_url;
+            }
+        }
+        done(null, { result: bookURL });
+    }
+
+    function productName(args, done)
+    {
+        var productId = args.productId;
+        var bookName = null; 
+        for(var i=0; i < mockData.length; i++)
+        {
+            if (mockData[i].product_id === productId)
+            {
+                bookName = mockData[i].product_name;
+            }
+        }
+        done(null, { result: bookName });
+    }
+}
---
+++ EX3/product-price-service/product_price.js
@@ -0,0 +1,23 @@
+
+
+module.exports = function (options) {
+    //Import the mock data json file
+    const mockData = require('./MOCK_DATA.json');
+    //To DO: Add the patterns and their corresponding functions
+    this.add('role:product,cmd:getProductPrice', productPrice);
+
+    //To DO: add the pattern functions and describe the logic inside the function
+    function productPrice(args, done)
+    {
+        let productId = args.productId;
+        let bookPrice = null; 
+        for(let i=0; i < mockData.length; i++)
+        {
+            if (mockData[i].product_id === productId)
+            {
+                bookPrice = mockData[i].product_price;
+            }
+        }
+        done(null, { result: bookPrice });
+    }
+}
